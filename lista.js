a3a35b2e4d31bab6e4fdefa942b2df978e5d053c
milenetoda <milene.toda@gmail.com>
2020-09-17 18:07:18 -0300

    somatoria

--- lista.js
+++ lista.js
@@ -91,42 +91,68 @@ function rodadas(dados) {
       i++;
 
       write(`
-    <div class="partida bloco">
-    <div class="bloco_numero">
-      <h3>Partida #${i}</h3>
-    </div>
+      <div class="partida bloco">
+        <div class="bloco_numero">
+          <h3>Partida #${i}</h3>
+        </div>
 
-    <ol class="bloco_itens resultados_partidas">
-    `)
-    for (const jogo of partida.jogos) {
-      criaJogo(dados, jogo);
-    }  
-    write(`
-    </ol>
-  </div>
-    `);
+        <ol class="bloco_itens resultados_partidas">
+      `)
+      var resultado = {
+        dupla1 : "",
+        pontos1 : 0,
+        dupla2 : "",
+        pontos2 : 0,
+      };
+      var contador = 0;
+      for (const jogo of partida.jogos) {
+        var nome1 = busca_participante(dados, jogo.jogador1_id).nome;
+        var nome2 = busca_participante(dados, jogo.jogador2_id).nome;
+        write(`<li>`)
+        criaJogo(nome1, jogo.pontos1, nome2, jogo.pontos2);
+        write(`</li>`)
+        if( contador === 0){
+          resultado.dupla1 += nome1;
+          resultado.dupla2 += nome2;
+        }
+        if (contador === 1){
+          resultado.dupla2 += "<br/>" + nome2;
+        }
+        if (contador === 2){
+          resultado.dupla1 += "<br/>" + nome1;
+        }
+        contador++;
+
+        resultado.pontos1 += parseInt(jogo.pontos1);
+        resultado.pontos2 += parseInt(jogo.pontos2);
+      }
+      write(`<li>`)
+      criaJogo(resultado.dupla1, resultado.pontos1, resultado.dupla2, resultado.pontos2);
+      write(`</li>`)
+      write(`
+        </ol>
+      </div>
+          `);      
     }
   }
 }
 
-function criaJogo(dados, jogo) {
-  var nome1 = busca_participante(dados, jogo.jogador1_id).nome;
-  var nome2 = busca_participante(dados, jogo.jogador2_id).nome;
-
+function criaJogo(nome1, pontos1, nome2, pontos2){
   write(`
-  <li>
+  <div class="jogo">
     <div class="jogador1">
-      <input class="resultado1-left" value="${jogo.pontos1}" readonly/>
+      <input class="resultado1-left" value="${pontos1}" readonly/>
       <span class="jogador-nome">${nome1}</span>
-      <input class="resultado1-right" value="${jogo.pontos1}" readonly/>
+      <input class="resultado1-right" value="${pontos1}" readonly/>
     </div>
     <div class="jogador-vs">X</div>
     <div class="jogador2">
-      <input class="resultado2" value="${jogo.pontos2}" readonly/>
+      <input class="resultado2" value="${pontos2}" readonly/>
       <span class="jogador-nome">${nome2}</span>
     </div>
-  </li>
+  </div>
   `)
+
 }
 
 function busca_participante (dados, id){
